9b5dd87b8fabeae70b2e450a73e10a67ec58c457
lucacasagrande <luca.casagrande@gmail.com>
2015-02-05 20:19:27 +0100

    Fix headers

--- __init__.py
+++ __init__.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
-"""
-/***************************************************************************
- OL3
-                                 A QGIS plugin
- Creates OpenLayers map from QGIS layers
-                             -------------------
-        begin                : 2014-04-25
-        copyright            : (C) 2014 by Victor Olaya
-        email                : volayaf@gmail.com
- ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
- This script initializes the plugin, making it known to QGIS.
-"""
+# qgis-ol3 Creates OpenLayers map from QGIS layers
+# Copyright (C) 2014 Victor Olaya (volayaf@gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 
 def classFactory(iface):
+
     from ol3 import OL3Plugin
     return OL3Plugin(iface)
